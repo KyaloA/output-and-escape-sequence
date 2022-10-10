c1304d93eccf3b70006047357293cfc2e26cbd77
KyaloA <allankyalo04@gmail.com>
2022-10-11 00:18:06 +0300

    First Commit

---
+++ Stickpeople Assignment.cpp
@@ -0,0 +1,15 @@
+/*Programme in C++ 
+*it outputs a man and woman in stick figures
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout <<"\n";
+    cout << "('_')         >('-')<\n";
+    cout << " /|\\           _/|\\_   \n";
+    cout << "/ | \\           / \\\n";
+    cout << " / \\           /___\\\n";
+    cout << "/_  \\_         _|_|\n";
+    return 0;
+}
